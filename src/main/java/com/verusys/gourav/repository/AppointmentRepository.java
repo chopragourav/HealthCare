5a7cb321afdfffe4421ebbd0a94a7c9d8cbfc078
Gourav Chopra <chprgourav@gmail.com>
2021-11-12 12:07:55 +0530

    Postgres

--- src/main/java/com/verusys/gourav/repository/AppointmentRepository.java
+++ src/main/java/com/verusys/gourav/repository/AppointmentRepository.java
@@ -9,18 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import com.verusys.gourav.entity.Appointment;
 
 /**
- * @author:GOURAV CHOPRA 
- *  Generated F/w:SHWR-Framework 
+ * @author:GOURAV CHOPRA Generated F/w:SHWR-Framework
  */
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-	
-	@Query("SELECT aptm.date, aptm.noOfSlots, aptm.fee, aptm.id FROM Appointment aptm INNER JOIN aptm.doctor as doctor WHERE doctor.id=:docId AND aptm.noOfSlots>0 AND aptm.date>= current_date")
+
+	@Query("SELECT aptm.date, aptm.noOfSlots, aptm.fee, aptm.id FROM Appointment aptm INNER JOIN aptm.doctor as doctor WHERE doctor.id=:docId AND aptm.noOfSlots>0 AND aptm.date>current_date")
 	public List<Object[]> getAppoinmentsByDoctor(Long docId);
-	
+
 	@Query("SELECT aptm.date, aptm.noOfSlots, aptm.fee, aptm.details FROM Appointment aptm INNER JOIN aptm.doctor as doctor WHERE doctor.email=:userName AND aptm.noOfSlots>0")
 	public List<Object[]> getAppoinmentsByDoctorEmail(String userName);
-	
+
 	@Modifying
 	@Query("UPDATE Appointment SET noOfSlots = noOfSlots + :count WHERE id=:id")
-	void updateSlotCountForAppoinment(Long id,int count);
+	void updateSlotCountForAppoinment(Long id, int count);
 }
